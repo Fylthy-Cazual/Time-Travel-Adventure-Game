baced664de5707be1f836c327108c9a50f956ba5
Nick-Sab <nsaballos486@gmail.com>
2023-12-04 17:27:18 -0800

    Added the chasing Enemy level

---
+++ Assets/Scripts/Enemies/ChasingEnemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChasingEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "Obstacle") {
+            Destroy(collision.gameObject);
+        }
+    }
+}
